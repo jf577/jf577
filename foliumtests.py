5a0d1d5e381dabc1a19241094683d22625343cbb
jf577 <joshfranklyn15@gmail.com>
2021-06-05 08:23:44 +1200

    updating

--- foliumtests.py
+++ foliumtests.py
@@ -5,3 +5,4 @@ m = folium.Map(location=[44.9794, -93.2761], zoom_start=9)
 # python foliumtests.py in terminal to obtain html file
 m.save("map1.html")
 
+
---
+++ testplots.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [0.1, 0.3, 0.6, 1.0, 1.4]
+plt.plot(x, y)
+plt.show()
+
