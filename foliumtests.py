e35a75bf16bf24df891db8ef83d3a4047a9673ac
jf577 <joshfranklyn15@gmail.com>
2021-06-04 22:54:59 +1200

    working on folium map

--- foliumtests.py
+++ foliumtests.py
@@ -1,3 +1,7 @@
 import folium
 
-testmap = folium.Map(location=[51.521, -0.713])
+m = folium.Map(location=[44.9794, -93.2761], zoom_start=9)
+
+# python foliumtests.py in terminal to obtain html file
+m.save("map1.html")
+
